28fe2ef1f94abadccddeb2f8400623da9a7157ba
Farzinkh <farzin.mag@gmail.com>
2023-02-16 19:27:11 +0330

    plotter fix

--- logs/SVR/plotter.py
+++ logs/SVR/plotter.py
@@ -34,20 +34,20 @@ for l in lines:
             rmselist.append(float(l[20:]))                    
         counter=counter+1
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(60, 30))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(60, 35))
   
 ax1.plot(namelist,xlist,linewidth=15,label='X')    
 ax1.plot(namelist,ylist,dashes=[2, 2, 10, 2],linewidth=15,label='Y')
 ax1.plot(namelist,zlist,'--',dashes=(2, 1),linewidth=15,label='Z')
 ax1.set_xticklabels(namelist, rotation=60, ha='right')
-ax1.legend()
+ax1.legend(prop={'size': 80})
 ax1.grid()
-ax1.set_ylabel('Coefficient of Determination',fontsize=36)
-ax1.set_xlabel('Case study and samplerates',fontsize=36)
+ax1.set_ylabel('Coefficient of Determination',fontsize=80)
+ax1.set_xlabel('Case study and samplerates',fontsize=80)
 
 ax2.plot(namelist,rmselist,linewidth=15)    
 ax2.set_xticklabels(namelist, rotation=60, ha='right')
 ax2.grid()
-ax2.set_ylabel('RSME (mm)',fontsize=36)
-ax2.set_xlabel('Case study and samplerates',fontsize=36)      
-plt.savefig('SVR_samplerate.png', dpi=300)     
+ax2.set_ylabel('RSME (mm)',fontsize=80)
+ax2.set_xlabel('Case study and samplerates',fontsize=80)    
+plt.savefig('SVR_samplerate.png', dpi=150)     
